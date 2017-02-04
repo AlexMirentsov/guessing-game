7f3a3ae91b00f7b33b0cdbfc72de1bf31cd051cb
AlexMirentsov <alexmirentsov@gmail.com>
2017-02-05 00:42:50 +0300

    lastcommit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var _min;
+        var _max;
+        var _number;
+    }
 
     setRange(min, max) {
-
+        this._max = max;
+        this._min = min;
     }
 
     guess() {
-
+        this._number = Math.ceil((this._min + this._max) / 2);
+        return this._number;
     }
 
     lower() {
-
+        this._max = this._number;
     }
 
     greater() {
 
+        this._min = this._number;
     }
 }
 
